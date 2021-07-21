26285e93bd18322b53664eb1bd3828b18866454f
Anasscodes <rockbell8@gmail.com>
2021-07-21 10:05:23 +0300

    Add basic ToDo functionalities

--- src/index.js
+++ src/index.js
@@ -3,18 +3,41 @@ import './style.css';
 
 let ToDo = [
     {
-        todo: "walk the dog",
+        Description: "walk the dog",
         completed: false,
         number:0
     },
     {
-        todo: "assignement work",
+        Description: "assignement work",
         completed: false,
         number:1
     }
 
 ];
 
-functio
+function RenderToDo(todo) {
+    const list = document.querySelector('todos-list');
+    const TodoContainer =  document.createElement('li');
+    const Checked = todo.completed ? 'done' : '';
+    TodoContainer.setAttribute('class', `todo-item ${Checked}`);
+    TodoContainer.setAttribute('data-index-number', todo.number);
+    TodoContainer.innerHTML = `
+    <div class="left flex">
+      <input class="check" id="${todo.number}" type="checkbox"/>
+      <label for="${todo.number}" class="tick js-tick"></label>
+      <span>${todo.Description}</span>
+    </div>
+    `
+    list.appendChild(TodoContainer);
+}
 
+function AddToDo(Description) {
+    const todo = {
+        Description,
+        completed: false,
+        number: ToDo.length,
+    };
+    ToDo.push(todo);
+    RenderToDo(todo);
+}
 
