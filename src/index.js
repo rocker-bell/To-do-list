727a2860d486710922d1f9eafcb4082968f08626
Anasscodes <rockbell8@gmail.com>
2021-07-21 08:18:37 +0300

    Add HTML/js basic code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import _ from 'lodash';
-function component() {
-    const element = document.createElement('div');
-  
-     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import './style.css';
 
-let ToDo = [{number: 1, Description: "washe the dishes", completed: false}, {number:2, Description: "programming task", completed: false }, {number: 3, Description: "walk the dog", completed: false}]
+let ToDo = [
+    {
+        todo: "walk the dog",
+        completed: false,
+        number:0
+    },
+    {
+        todo: "assignement work",
+        completed: false,
+        number:1
+    }
 
-console.log(ToDo)
+];
 
-console.log(ToDo[0])
+functio
 
-  ToDo = {number: , Description: , completed: }
+
